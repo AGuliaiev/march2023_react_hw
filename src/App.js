509e30096d0e7b4c8762dee1b0aad434fc36553f
gulini32 <gulini32@gmail.com>
2023-07-05 11:18:24 +0300

    hw1 part2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Character from "./components/character/Character";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <div>
+            <Character id={1} name={'Rick Sanchez'} status={"Alive"} species={"Human"} gender={"Male"} image={'https://rickandmortyapi.com/api/character/avatar/1.jpeg'}/>
+            <Character id={425} name={'Pizza-person'} status={"Alive"} species={"Humanoid"} gender={"Male"} image={'https://rickandmortyapi.com/api/character/avatar/425.jpeg'}/>
+            <Character id={582} name={'Anchosnake'} status={"Alive"} species={"Animal"} gender={"Female"} image={"https://rickandmortyapi.com/api/character/avatar/582.jpeg"}/>
+            <Character id={28} name={"Attila Starwar"} status={"Alive"} species={"Alien"} gender={"Male"} image={"https://rickandmortyapi.com/api/character/avatar/28.jpeg"}/>
+            <Character id={765} name={'Native Alien'} status={"Alive"} species={"Human"} gender={"Male"} image={"https://rickandmortyapi.com/api/character/avatar/765.jpeg"}/>
+            <Character id={84} name={'Cult Leader Morty'} status={"Alive"} species={"Human"} gender={"Male"} image={"https://rickandmortyapi.com/api/character/avatar/84.jpeg"}/>
+
+
+        </div>
+    );
+};
 
 export default App;
