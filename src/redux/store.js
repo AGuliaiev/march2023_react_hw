e05e065859b6d95c9fa3e84b19ea15d91d1bcfab
gulini32 <gulini32@gmail.com>
2023-08-10 14:26:03 +0300

    rick_and_morty

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,19 @@
 import {combineReducers, createStore} from "redux";
 import {charactersReducer, episodesReducer} from "./reducers";
+import {composeWithDevTools} from "redux-devtools-extension";
 
 const rootReducer = combineReducers({
     episodes: episodesReducer,
     characters: charactersReducer
 });
-const store = createStore(rootReducer);
+
+const composeEnhancers = composeWithDevTools({
+    trace: true,
+    traceLimit: 25
+})
+
+
+const store = createStore(rootReducer, composeEnhancers());
 
 export {
     store
